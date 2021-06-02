7cda66daea85c19033b57ebf2f77e6b5e1a03fed
Aleksey Metelskiy <alnoroid@gmail.com>
2021-06-02 23:16:03 +0300

    updated gemlist

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 #lock "~> 3.15.0"
 
-set :application, "carso_sait_new"
-set :repo_url, "git@bitbucket.org:andreevmap_work/carso_sait_new.git"
+set :application, "autosalon_m"
+set :repo_url, "git@bitbucket.org:andreevmap_work/autosalon_m.git"
 
 set :linked_files, %w{config/master.key config/database.yml}
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets','public/uploads','public/images','public/sitemaps','node_modules','bundle')
@@ -11,9 +11,9 @@ set :migration_role, :app
 set :rbenv_type, :user
 set :rbenv_ruby, "2.5.3"
 
-set :rbenv_path, '/home/carso_master/.rbenv/'
+set :rbenv_path, '/home/khan/.rbenv'
 
-set :puma_restart_command, 'bundle exec --keep-file-descriptors puma'
+# set :puma_restart_command, 'bundle exec --keep-file-descriptors puma'
 
-after :deploy, "puma:stop"
-after :deploy, "puma:start"
+# after :deploy, "puma:stop"
+# after :deploy, "puma:start"
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,5 @@
 # after 'deploy:published', 'sitemap:refresh'
-after 'deploy:published', 'data:clear'
+# after 'deploy:published', 'data:clear'
 
 set :stage, "development"
 set :rails_env, :development
@@ -8,6 +8,6 @@ set :bundle_path, 'bundle'
 set :bundle_flags, '--quiet'
 set :bundle_without, %w{test}.join(' ')
 
-set :deploy_to, "/var/www/carso_new_staging"
+set :deploy_to, "/var/www/autosalon_staging"
 
-server "45.128.207.158", user: "carso_master", roles: %w{web app}, port: 22
+server "62.109.5.245", user: "khan", roles: %w{web app}, port: 32221
