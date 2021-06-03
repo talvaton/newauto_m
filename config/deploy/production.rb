12db7cc3602973dd70f86dcb8dc656cd74ff667b
Aleksey Metelskiy <alnoroid@gmail.com>
2021-06-03 23:35:32 +0300

    cap production and robots

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,13 +1,13 @@
-after 'deploy:published', 'data:database'
-after 'deploy:published', 'data:clear'
+#after 'deploy:published', 'data:database'
+#after 'deploy:published', 'data:clear'
 
 set :stage, "production"
-#set :rails_env, :production
+set :rails_env, :production
 
 set :bundle_path, 'bundle'
 set :bundle_flags, '--quiet'
 set :bundle_without, %w{test}.join(' ')
 
-set :deploy_to, "/var/www/carso_new_production"
+set :deploy_to, "/var/www/autosalon_production"
 
-server "45.128.207.158", user: "carso_master", roles: %w{web app}, port: 22
+server "62.109.5.245", user: "khan", roles: %w{web app}, port: 32221
