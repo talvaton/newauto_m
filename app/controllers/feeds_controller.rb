ad2452f2fb56a5fd23dd504dedb898064c4c3f32
finderfun <finderfun.ru@gmail.com>
2021-09-03 15:50:19 +0300

    front

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -4,7 +4,8 @@ class FeedsController < ApplicationController
   end
 
   def yml_export
-    @brands = Brand.all
+    allowed_ids = [4,10,13,14,15,16,18,21,22,23,25,26,27,28,32,36]
+    @brands = Brand.where(id: allowed_ids).where(menu_show: true).order(:title)
     @new = NewCar.where(hide: 0)
     @used = UsedCar.used_cars
   end
@@ -35,12 +36,14 @@ class FeedsController < ApplicationController
   end
 
   def xml_yandex
-    @brands = Brand.all
+    allowed_ids = [4,10,13,14,15,16,18,21,22,23,25,26,27,28,32,36]
+    @brands = Brand.where(id: allowed_ids).where(menu_show: true).order(:title)
     @new = NewCar.where(hide: 0)
     @newcars = NewCar.where(feed: 1).where(hide: 0).where(available: 1).includes(:brand)
   end
   def xml_vk
-    @brands = Brand.all
+    allowed_ids = [4,10,13,14,15,16,18,21,22,23,25,26,27,28,32,36]
+    @brands = Brand.where(id: allowed_ids).where(menu_show: true).order(:title)
     @new = NewCar.where(hide: 0)
     @newcars = NewCar.where(feed: 1).where(hide: 0).where(available: 1).includes(:brand)
   end
