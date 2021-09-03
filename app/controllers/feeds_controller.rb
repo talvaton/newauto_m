38d4bb6c83a43d47ca9da86a95c85530452f32e2
alnor <alnoroid@gmail.com>
2021-09-03 17:33:01 +0300

    fix for yandex feed

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -29,18 +29,15 @@ class FeedsController < ApplicationController
 
   def xml_target
     @brands = Brand.where(id: ALLOWED_BRANDS).where(menu_show: true).order(:title)
-    #@brands = Brand.all
     @newcars = NewCar.where(feed: 1).where(hide: 0).where(available: 1).where(brand_id: ALLOWED_BRANDS).includes(:brand)
   end
 
   def xml_yandex
     @brands = Brand.where(id: ALLOWED_BRANDS).where(menu_show: true).order(:title)
-    @new = NewCar.where(hide: 0)
     @newcars = NewCar.where(feed: 1).where(hide: 0).where(available: 1).where(brand_id: ALLOWED_BRANDS).includes(:brand)
   end
   def xml_vk
     @brands = Brand.where(id: ALLOWED_BRANDS).where(menu_show: true).order(:title)
-    @new = NewCar.where(hide: 0)
     @newcars = NewCar.where(feed: 1).where(hide: 0).where(available: 1).where(brand_id: ALLOWED_BRANDS).includes(:brand)
   end
 
