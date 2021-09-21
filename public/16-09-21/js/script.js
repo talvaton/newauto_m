3fb97a9107da1adb15fcd5823a58c362aa02ac39
finderfun <finderfun.ru@gmail.com>
2021-09-21 14:40:34 +0300

    front

---
+++ public/16-09-21/js/script.js
@@ -0,0 +1,52 @@
+$('.main-pic-list-cont').slick({
+	slidesToShow: 3,
+    responsive: [
+
+        {
+            breakpoint: 1000,
+             settings: {
+                    slidesToShow: 1
+                }
+        }
+    ]
+});
+
+$('.catalog_item').on('click', (e) => {
+
+    $('.catalog-auto').toggleClass("show");
+});
+
+$('.main-equip-item').on('click', (e) => {
+
+    $('.main-equip-item').find(".main-equip-pop").toggleClass("show");
+});
+
+
+function burgerMenu(selector) {
+    let menu = $(selector);
+    let button = menu.find('.burger-menu__button');
+    let links = menu.find('.burger-menu__link');
+    let overlay = menu.find('.burger-menu__overlay')
+
+
+    button.on('click', (e) => {
+        e.preventDefault();
+        toggleMenu();
+    });
+
+    links.on('click', () => toggleMenu());
+    overlay.on('click', () => toggleMenu()); 
+
+    function toggleMenu () {
+        menu.toggleClass('burger-menu_active');
+
+        if (menu.hasClass('burger-menu_active')) {
+            $('body').css('overflow', 'hidden');
+        } else {
+            $('body').css('overflow', 'visible');
+        }
+    }
+
+}  
+burgerMenu('.burger-menu');
+
