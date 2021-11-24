af4549a99caa02abdeac7d94df557e9611993cce
finderfun <finderfun.ru@gmail.com>
2021-11-24 03:50:10 +0300

    trad in

--- app/assets/javascripts/carso/carso_tradein.js
+++ app/assets/javascripts/carso/carso_tradein.js
@@ -13,7 +13,7 @@ window.onload = function() {
     // Give the URL parameters variable names
     var source = getParameterByName('utm_source');
 //    var medium = getParameterByName('utm_medium');
-//    var campaign = getParameterByName('utm_campaign');
+    var campaign = getParameterByName('utm_term').replace("_trade-in", " ");
 
     // Put the variable names into the hidden fields in the form.
     //document.getElementById("select2-credit_ticket_other_marka-container").value = source;
@@ -22,7 +22,7 @@ window.onload = function() {
 
     //U Can use it with Expertsender Forms
 
-    var $option = $("<span class='select2-selection__rendered' id='select2-credit_ticket_other_marka-container' role='textbox' aria-readonly='true' title=''></span>").val('1').text(source);
+    var $option = $("<span class='select2-selection__rendered' id='select2-credit_ticket_other_marka-container' role='textbox' aria-readonly='true' title=''></span>").val('1').text(campaign);
     //var $optionm = $("<span class='select2-selection__rendered' id='select2-credit_ticket_other_model-container' role='textbox' aria-readonly='true' title='Picanto'>Picanto</span>").val('1').text("Picanto");
     $('#select2-credit_ticket_other_marka-container').html($option).trigger('change');
   //  $('#select2-credit_ticket_other_model-container').html($optionm).trigger('change').parent().parent().parent().removeClass("select2-container--disabled").addClass("select2-container--below");
